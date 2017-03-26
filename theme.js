3967e91fe8321829cc1b43e157a74e8d1e5c94e9
kkayyoo <yokoyau159@gmail.com>
2017-03-26 16:33:06 -0400

    Adding slider script

--- theme.js
+++ theme.js
@@ -52,4 +52,40 @@ $(function(){
      };
   });
 
+  //slider
+
+    var width = 840;
+    var animationSpeed = 1500;
+    var pause = 3000;
+    var currentSlide = 1;
+
+    var $slider = $('#slider');
+    var $slideContainer = $slider.find('.slides');
+    var $slides = $slideContainer.find('.slide');
+
+    var interval;
+
+    function startSlider(){
+        interval = setInterval(function(){ //animate margin-left
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function(){
+                currentSlide++;
+                if (currentSlide > $slides.length){
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);   //If it's last slide, go back
+             }
+          });
+        }, pause);
+    }
+
+    function stopSlider(){
+        clearInterval(interval);
+    }
+
+    //listen for mouseenter and pause
+    //resume on mouseleave
+    $slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
+
+    startSlider();
+
+
 })
